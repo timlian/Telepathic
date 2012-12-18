272f15a9d85c3801d9bd7d3851391c9972453932
tim lian <timlian@yeah.net>
2012-12-18 16:25:08 +0800

    Add splash screen.

---
+++ Projects/Finder/src/com/telepathic/finder/app/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.telepathic.finder.app;
+
+import com.telepathic.finder.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+    
+    private static final int SPLASH_DISPLAY_LENGHT = 1500;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_view);
+        // New Handler to start the HomeActivity and close this SplashActivity after some seconds.
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this, HomeActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGHT);
+    }
+}
