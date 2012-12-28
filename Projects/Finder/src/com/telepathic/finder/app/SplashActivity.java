41f237d4bc16a4bfcb13e6bd30e98dbfa1ca139e
lsyiverson <lsyiverson@gmail.com>
2012-12-28 18:04:11 +0800

    Use the TabActivity to contain the function page

--- Projects/Finder/src/com/telepathic/finder/app/SplashActivity.java
+++ Projects/Finder/src/com/telepathic/finder/app/SplashActivity.java
@@ -1,16 +1,16 @@
 package com.telepathic.finder.app;
 
-import com.telepathic.finder.R;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.telepathic.finder.R;
+
 public class SplashActivity extends Activity {
-    
+
     private static final int SPLASH_DISPLAY_LENGHT = 1000 * 2;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +19,7 @@ public class SplashActivity extends Activity {
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
-                Intent mainIntent = new Intent(SplashActivity.this, HomeActivity.class);
+                Intent mainIntent = new Intent(SplashActivity.this, TabHomeActivity.class);
                 startActivity(mainIntent);
                 finish();
             }
