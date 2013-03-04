903414ccb233654779ca72cc02bac9f925e0db19
lsyiverson <lsyiverson@gmail.com>
2013-03-04 15:21:59 +0800

    Use icon in navigation tab

--- Projects/Finder/src/com/telepathic/finder/app/BusLocationFragment.java
+++ Projects/Finder/src/com/telepathic/finder/app/BusLocationFragment.java
@@ -1,18 +1,64 @@
 package com.telepathic.finder.app;
 
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockFragment;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.widget.SearchView;
+import com.actionbarsherlock.widget.SearchView.OnQueryTextListener;
 import com.telepathic.finder.R;
 
 public class BusLocationFragment extends SherlockFragment {
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.bus_location_view, container, false);
     }
 
+    @Override
+    public void onStart() {
+        ActionBar actionBar = getSherlockActivity().getSupportActionBar();
+        actionBar.setTitle(R.string.bus_location);
+        super.onStart();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // Inflate the options menu from XML
+        inflater.inflate(R.menu.menu_bus_location, menu);
+
+        // Get the SearchView and set the searchable configuration
+        SearchView searchView = (SearchView) menu.findItem(R.id.search_bus_location).getActionView();
+        searchView.setQueryHint(getResources().getText(R.string.bus_number_hint));
+        searchView.setInputType(InputType.TYPE_CLASS_TEXT);
+        searchView.setOnQueryTextListener(new OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                //                mTvSearchKey.setText(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // TODO Auto-generated method stub
+                return false;
+            }
+        });
+        super.onCreateOptionsMenu(menu, inflater);
+    }
 }
--- Projects/Finder/src/com/telepathic/finder/app/BusStationFragment.java
+++ Projects/Finder/src/com/telepathic/finder/app/BusStationFragment.java
@@ -1,17 +1,65 @@
 package com.telepathic.finder.app;
 
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockFragment;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.widget.SearchView;
+import com.actionbarsherlock.widget.SearchView.OnQueryTextListener;
 import com.telepathic.finder.R;
 
 public class BusStationFragment extends SherlockFragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.bus_station, container, false);
     }
+
+    @Override
+    public void onStart() {
+        ActionBar actionBar = getSherlockActivity().getSupportActionBar();
+        actionBar.setTitle(R.string.bus_stations);
+        super.onStart();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // Inflate the options menu from XML
+        inflater.inflate(R.menu.menu_bus_station, menu);
+
+        // Get the SearchView and set the searchable configuration
+        SearchView searchView = (SearchView) menu.findItem(R.id.search_bus_station).getActionView();
+        searchView.setQueryHint(getResources().getText(R.string.station_number_hint));
+        searchView.setInputType(InputType.TYPE_CLASS_NUMBER);
+        searchView.setOnQueryTextListener(new OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                //                mTvSearchKey.setText(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // TODO Auto-generated method stub
+                return false;
+            }
+        });
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+
 }
