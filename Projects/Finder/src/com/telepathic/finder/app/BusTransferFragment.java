a31d6febfc77562fcf7fdd4aaca9b8e248608af3
lsyiverson <lsyiverson@gmail.com>
2013-04-05 22:52:50 +0800

    Display the bus transfer information

--- Projects/Finder/src/com/telepathic/finder/app/BusTransferFragment.java
+++ Projects/Finder/src/com/telepathic/finder/app/BusTransferFragment.java
@@ -3,22 +3,26 @@ package com.telepathic.finder.app;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.Html;
 import android.text.TextUtils;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.AutoCompleteTextView;
+import android.widget.BaseAdapter;
 import android.widget.Filter;
 import android.widget.Filterable;
 import android.widget.ImageButton;
 import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,19 +32,31 @@ import com.telepathic.finder.sdk.ICompletionListener;
 import com.telepathic.finder.sdk.ITrafficService;
 import com.telepathic.finder.sdk.traffic.entity.kuaixin.KXProgramStep;
 import com.telepathic.finder.sdk.traffic.entity.kuaixin.KXTransferProgram;
+import com.telepathic.finder.util.Utils;
 
 public class BusTransferFragment extends SherlockFragment {
+
+    private static final int CUSTOM_DIALOG_ID_START = 100;
+
+    private static final int BUS_TRANSFER_SEARCH_DLG = CUSTOM_DIALOG_ID_START + 1;
+
     private MainActivity mActivity;
 
     private AutoCompleteTextView mStartStation;
 
     private AutoCompleteTextView mEndStation;
 
+    private ListView mTransferList;
+
     private ImageButton mBtnQueryTransfer;
 
     private ITrafficService mTrafficService;
 
+    private Dialog mDialog;
+
     private String mKeyWord;
+
+    private ProgressBar mStartStationProgressBar, mEndStationProgressBar;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -61,6 +77,10 @@ public class BusTransferFragment extends SherlockFragment {
         FinderApplication app = (FinderApplication)mActivity.getApplication();
         mTrafficService = app.getTrafficService();
 
+        setupView();
+    }
+
+    private void setupView() {
         mStartStation = (AutoCompleteTextView)getView().findViewById(R.id.start_station);
         mStartStation.setThreshold(2);
         mStartStation.addTextChangedListener(new StationNameWatcher(mStartStation));
@@ -76,61 +96,101 @@ public class BusTransferFragment extends SherlockFragment {
                 String startStationName = mStartStation.getText().toString().trim();
                 String endStationName = mEndStation.getText().toString().trim();
                 if (TextUtils.isEmpty(startStationName) || TextUtils.isEmpty(endStationName)) {
-                    //TODO: Need add resource
-                    Toast.makeText(mActivity, "Please input the station name", Toast.LENGTH_SHORT);
+                    Toast.makeText(mActivity, R.string.no_station_name_tips, Toast.LENGTH_SHORT).show();
                 } else {
-                    mTrafficService.getBusTransferRoute(startStationName, endStationName, new ICompletionListener() {
-                        @Override
-                        public void onSuccess(Object result) {
-                            ArrayList<KXTransferProgram> transferList = (ArrayList<KXTransferProgram>)result;
-                            for (KXTransferProgram program : transferList) {
-                                String id = program.getProgramId();
-                                String time = program.getTransferTime();
-                                List<KXProgramStep> steps = program.getSteps();
-                                Log.d("debug", "id = " + id);
-                                Log.d("debug", "time = " + time);
-                                for (KXProgramStep step : steps) {
-                                    Log.d("debug", "====from = " + step.getSource());
-                                    Log.d("debug", "====by = " + step.getLineName());
-                                    Log.d("debug", "====to = " + step.getDestination());
-                                }
-                            }
-                        }
-
-                        @Override
-                        public void onFailure(int errorCode, String errorText) {
-                            // TODO Auto-generated method stub
-
-                        }
-                    });
+                    showDialog(BUS_TRANSFER_SEARCH_DLG);
+                    getBusTransferRoute(startStationName, endStationName);
+                    Utils.hideSoftKeyboard(mActivity, mEndStation);
                 }
+            }
+        });
+
+        mTransferList = (ListView)getView().findViewById(R.id.transfer_list);
+
+        mStartStationProgressBar = (ProgressBar)getView().findViewById(R.id.start_station_progressbar);
+        mEndStationProgressBar = (ProgressBar)getView().findViewById(R.id.end_station_progressbar);
+    }
+
+    private void getBusTransferRoute(String startStation, String endStation) {
+        mTrafficService.getBusTransferRoute(startStation, endStation, new ICompletionListener() {
+            @Override
+            public void onSuccess(Object result) {
+                dismissWaittingDialog();
+                ArrayList<KXTransferProgram> transferList = (ArrayList<KXTransferProgram>)result;
+                TransferSchemeAdapter adapter = new TransferSchemeAdapter(transferList);
+                mTransferList.setAdapter(adapter);
+            }
+
+            @Override
+            public void onFailure(int errorCode, String errorText) {
+                dismissWaittingDialog();
+                String reason = Utils.getErrorMessage(getResources(), errorCode, errorText);
+                String description = getString(R.string.get_bus_transfer_failed, reason);
+                Toast.makeText(mActivity, description, Toast.LENGTH_SHORT).show();
             }
         });
     }
 
     private void getSuggestionStations(final AutoCompleteTextView stationEditText) {
         if (mKeyWord.length() >= 2) {
+            if (stationEditText == mStartStation) {
+                mStartStationProgressBar.setVisibility(View.VISIBLE);
+            } else if (stationEditText == mEndStation) {
+                mEndStationProgressBar.setVisibility(View.VISIBLE);
+            }
             mTrafficService.queryStationName(mKeyWord, new ICompletionListener() {
 
                 @Override
                 public void onSuccess(Object result) {
                     ArrayList<String> stations = (ArrayList<String>)result;
                     if (stations != null && stations.size() > 0) {
-                        for(String station : stations) {
-                            Log.d("debug", "lisy=======station = " + station);
+                        if (!(stations.size() == 1 && stations.get(0).equals(mKeyWord))) {
+                            StationNameAdapter adapter = new StationNameAdapter(stations);
+                            stationEditText.setAdapter(adapter);
+                            stationEditText.showDropDown();
                         }
-                        StationNameAdapter adapter = new StationNameAdapter(stations);
-                        stationEditText.setAdapter(adapter);
-                        stationEditText.showDropDown();
                     }
-
+                    if (stationEditText == mStartStation) {
+                        mStartStationProgressBar.setVisibility(View.GONE);
+                    } else if (stationEditText == mEndStation) {
+                        mEndStationProgressBar.setVisibility(View.GONE);
+                    }
                 }
 
                 @Override
                 public void onFailure(int errorCode, String errorText) {
-                    // TODO Auto-generated method stub
+                    if (stationEditText == mStartStation) {
+                        mStartStationProgressBar.setVisibility(View.GONE);
+                    } else if (stationEditText == mEndStation) {
+                        mEndStationProgressBar.setVisibility(View.GONE);
+                    }
                 }
             });
+        }
+    }
+
+    private void showDialog(int id) {
+        mDialog = null;
+        switch (id) {
+            case BUS_TRANSFER_SEARCH_DLG:
+                ProgressDialog prgDlg = new ProgressDialog(mActivity);
+                prgDlg.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                prgDlg.setMessage(getResources().getString(R.string.find_transfer_information));
+                prgDlg.setIndeterminate(true);
+                prgDlg.setOnCancelListener(null);
+                mDialog = prgDlg;
+                break;
+            default:
+                break;
+        }
+        if (mDialog != null) {
+            mDialog.show();
+        }
+    }
+
+    private void dismissWaittingDialog() {
+        if (mDialog != null && mDialog.isShowing()) {
+            mDialog.dismiss();
         }
     }
 
@@ -285,6 +345,72 @@ public class BusTransferFragment extends SherlockFragment {
 
         }
 
+    }
+
+    private class TransferSchemeAdapter extends BaseAdapter {
+
+        private ArrayList<KXTransferProgram> mTransferList;
+
+        public TransferSchemeAdapter(ArrayList<KXTransferProgram> transferList) {
+            mTransferList = transferList;
+        }
+
+        @Override
+        public int getCount() {
+            return mTransferList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mTransferList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            KXTransferProgram program = mTransferList.get(position);
+            if (convertView == null) {
+                convertView = mActivity.getLayoutInflater().inflate(R.layout.bus_transfer_scheme_item, null);
+                TransferProgramHolder holder = new TransferProgramHolder();
+                holder.mTransferTitle = (TextView)convertView.findViewById(R.id.transfer_title);
+                holder.mTransferDetail = (TextView)convertView.findViewById(R.id.transfer_detail);
+                convertView.setTag(holder);
+            }
+            bindView(program, convertView);
+            return convertView;
+        }
+
+        private void bindView(KXTransferProgram program, View view) {
+            TransferProgramHolder holder = (TransferProgramHolder)view.getTag();
+            String id = program.getProgramId();
+            String transferCount = program.getTransferTime();
+            StringBuilder transferInfo = new StringBuilder();
+            StringBuilder transferDetail = new StringBuilder();
+            List<KXProgramStep> steps = program.getSteps();
+            for (int i=0; i < steps.size(); i++) {
+                KXProgramStep step = steps.get(i);
+                if (i == 0) {
+                    transferInfo.append(step.getLineName());
+                    transferDetail.append(mActivity.getString(R.string.transfer_scheme_detail, step.getSource(), step.getLineName(), step.getDestination()));
+                } else {
+                    transferInfo.append(mActivity.getResources().getString(R.string.transfer_arrow) + step.getLineName());
+                    transferDetail.append("<br/>" + mActivity.getString(R.string.transfer_scheme_detail, step.getSource(), step.getLineName(), step.getDestination()));
+                }
+            }
+            String title = mActivity.getString(R.string.transfer_scheme_title, id, transferCount, transferInfo.toString());
+            String detail = transferDetail.toString();
+            holder.mTransferTitle.setText(title);
+            holder.mTransferDetail.setText(Html.fromHtml(detail));
+        }
+
+        private class TransferProgramHolder {
+            TextView mTransferTitle;
+            TextView mTransferDetail;
+        }
     }
 
 
--- Projects/Finder/src/com/telepathic/finder/sdk/traffic/request/GetStationNameRequest.java
+++ Projects/Finder/src/com/telepathic/finder/sdk/traffic/request/GetStationNameRequest.java
@@ -6,53 +6,53 @@ import org.ksoap2.serialization.SoapObject;
 
 public class GetStationNameRequest extends RPCBaseRequest {
 
-	private static final String METHOD_NAME = "stationFuzzyQuery";
-	private static final String KEY_PAGE_INDEX = "pageIndex";
-	private static final String KEY_PAGE_SIZE = "pageSize";
-	private static final String KEY_QUERY = "query";
-	
-	private static final int PAGE_INDEX = 1;
-	private static final int PAGE_SIZE = 100;
-	private static final int MAX_STATION_NUM = PAGE_SIZE;
-	
-	//response keys
-	private static final String KEY_TOTAL_NUMBER = "totalNum";
-	private static final String KEY_STATION_NAME = "station";
-	
-	public GetStationNameRequest(String query) {
-		super(METHOD_NAME);
-		addParameter(KEY_PAGE_INDEX, PAGE_INDEX);
-		addParameter(KEY_PAGE_SIZE, PAGE_SIZE);
-		addParameter(KEY_QUERY, query);
-	}
+    private static final String METHOD_NAME = "stationFuzzyQuery";
+    private static final String KEY_PAGE_INDEX = "pageIndex";
+    private static final String KEY_PAGE_SIZE = "pageSize";
+    private static final String KEY_QUERY = "query";
 
-	@Override
-	void handleError(int errorCode, String errorMessage) {
-		if (mCallback != null) {
-			mCallback.onError(errorCode, errorMessage);
-		}
-	}
+    private static final int PAGE_INDEX = 1;
+    private static final int PAGE_SIZE = 100;
+    private static final int MAX_STATION_NUM = PAGE_SIZE;
 
-	@Override
-	void handleResponse(SoapObject dataSet) {
-		int totalNum = 0;
-		SoapObject dataEntry = null;
-		if (dataSet != null && dataSet.getPropertyCount() > 1) {
-			dataEntry = (SoapObject)dataSet.getProperty(0);
-			totalNum = Integer.parseInt((dataEntry.getPrimitivePropertyAsString(KEY_TOTAL_NUMBER)));
-			if (totalNum > MAX_STATION_NUM) {
-				totalNum = MAX_STATION_NUM;
-			}
-		}
-		ArrayList<String> stationNames = new ArrayList<String>();
-		for(int idx = 0; idx < totalNum; idx++) {
-			dataEntry = (SoapObject)dataSet.getProperty(idx);
-			String name = dataEntry.getPrimitivePropertyAsString(KEY_STATION_NAME);
-			stationNames.add(name);
-		}
-		if (mCallback != null) {
-			mCallback.onSuccess(stationNames);
-		}
-	}
+    //response keys
+    private static final String KEY_TOTAL_NUMBER = "totalNum";
+    private static final String KEY_STATION_NAME = "station";
+
+    public GetStationNameRequest(String query) {
+        super(METHOD_NAME);
+        addParameter(KEY_PAGE_INDEX, PAGE_INDEX);
+        addParameter(KEY_PAGE_SIZE, PAGE_SIZE);
+        addParameter(KEY_QUERY, query);
+    }
+
+    @Override
+    void handleError(int errorCode, String errorMessage) {
+        if (mCallback != null) {
+            mCallback.onError(errorCode, errorMessage);
+        }
+    }
+
+    @Override
+    void handleResponse(SoapObject dataSet) {
+        int totalNum = 0;
+        SoapObject dataEntry = null;
+        if (dataSet != null && dataSet.getPropertyCount() >= 1) {
+            dataEntry = (SoapObject)dataSet.getProperty(0);
+            totalNum = Integer.parseInt((dataEntry.getPrimitivePropertyAsString(KEY_TOTAL_NUMBER)));
+            if (totalNum > MAX_STATION_NUM) {
+                totalNum = MAX_STATION_NUM;
+            }
+        }
+        ArrayList<String> stationNames = new ArrayList<String>();
+        for(int idx = 0; idx < totalNum; idx++) {
+            dataEntry = (SoapObject)dataSet.getProperty(idx);
+            String name = dataEntry.getPrimitivePropertyAsString(KEY_STATION_NAME);
+            stationNames.add(name);
+        }
+        if (mCallback != null) {
+            mCallback.onSuccess(stationNames);
+        }
+    }
 
 }
