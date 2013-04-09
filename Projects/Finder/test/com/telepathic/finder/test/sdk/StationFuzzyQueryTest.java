b459a951ed5b26d5b3b8415c120bd8bdfe52df9a
lsyiverson <lsyiverson@gmail.com>
2013-04-09 11:33:07 +0800

    Add UT for get only one station name

--- Projects/Finder/test/com/telepathic/finder/test/sdk/StationFuzzyQueryTest.java
+++ Projects/Finder/test/com/telepathic/finder/test/sdk/StationFuzzyQueryTest.java
@@ -2,72 +2,90 @@ package com.telepathic.finder.test.sdk;
 
 import java.util.ArrayList;
 
+import android.test.ApplicationTestCase;
+import android.util.Log;
+
 import com.telepathic.finder.app.FinderApplication;
 import com.telepathic.finder.sdk.ICompletionListener;
 import com.telepathic.finder.sdk.ITrafficService;
 
-import android.test.ApplicationTestCase;
-import android.util.Log;
-
 public class StationFuzzyQueryTest extends ApplicationTestCase<FinderApplication> {
-	private static final String TAG = "StationFuzzyQueryTest";
-	
-	private FinderApplication mApp = null;
-	private ITrafficService mTrafficService = null;
-	private volatile boolean mIsDone;
-	private Object mLock = new Object();
+    private static final String TAG = "StationFuzzyQueryTest";
 
-	public StationFuzzyQueryTest(Class<FinderApplication> applicationClass) {
-		super(applicationClass);
-	}
+    private FinderApplication mApp = null;
+    private ITrafficService mTrafficService = null;
+    private volatile boolean mIsDone;
+    private Object mLock = new Object();
 
-	public StationFuzzyQueryTest() {
-		super(FinderApplication.class);
-	}
+    public StationFuzzyQueryTest(Class<FinderApplication> applicationClass) {
+        super(applicationClass);
+    }
 
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
-		createApplication();
-		mApp = getApplication();
-		mTrafficService = mApp.getTrafficService();
-	}
+    public StationFuzzyQueryTest() {
+        super(FinderApplication.class);
+    }
 
-	public void test_retrieve_station_name() {
-		mTrafficService.queryStationName("新会展", new ICompletionListener() {
-			@Override
-			public void onSuccess(Object result) {
-				ArrayList<String> stationNames = (ArrayList<String>) result;
-				assertEquals(5, stationNames.size());
-				notifyDone();
-			}
-			
-			@Override
-			public void onFailure(int errorCode, String errorText) {
-				Log.d(TAG, "onFailure: errorCode = " + errorCode + ", errorText: " + errorText);
-				notifyDone();
-			}
-		});
-		waitResponse();
-	}
-	
-	private void waitResponse() {
-		synchronized (mLock) {
-			try {
-				while(!mIsDone) {
-					mLock.wait();
-				}
-			} catch (InterruptedException e) {
-				Log.d(TAG, "Exception: " + e.getMessage());
-			}
-		}
-	}
-	
-	private void notifyDone() {
-		synchronized (mLock) {
-			mIsDone = true;
-			mLock.notifyAll();
-		}
-	}
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        createApplication();
+        mApp = getApplication();
+        mTrafficService = mApp.getTrafficService();
+    }
+
+    public void test_retrieve_station_name() {
+        mTrafficService.queryStationName("新会展", new ICompletionListener() {
+            @Override
+            public void onSuccess(Object result) {
+                ArrayList<String> stationNames = (ArrayList<String>) result;
+                assertEquals(5, stationNames.size());
+                notifyDone();
+            }
+
+            @Override
+            public void onFailure(int errorCode, String errorText) {
+                Log.d(TAG, "onFailure: errorCode = " + errorCode + ", errorText: " + errorText);
+                notifyDone();
+            }
+        });
+        waitResponse();
+    }
+
+    public void test_retrieve_station_name_single() {
+        mTrafficService.queryStationName("西门", new ICompletionListener() {
+            @Override
+            public void onSuccess(Object result) {
+                ArrayList<String> stationNames = (ArrayList<String>) result;
+                assertEquals(1, stationNames.size());
+                notifyDone();
+            }
+
+            @Override
+            public void onFailure(int errorCode, String errorText) {
+                Log.d(TAG, "onFailure: errorCode = " + errorCode + ", errorText: " + errorText);
+                notifyDone();
+            }
+        });
+        waitResponse();
+    }
+
+    private void waitResponse() {
+        synchronized (mLock) {
+            try {
+                while(!mIsDone) {
+                    mLock.wait();
+                }
+            } catch (InterruptedException e) {
+                Log.d(TAG, "Exception: " + e.getMessage());
+            }
+        }
+    }
+
+    private void notifyDone() {
+        synchronized (mLock) {
+            mIsDone = true;
+            mLock.notifyAll();
+        }
+    }
 
 }
